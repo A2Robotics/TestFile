a11974c45130152515a15491d1ab5aaf83c19c6f
Sandro Zenatti <info@a2robotics.com>
2021-07-31 19:09:41 +0200

    j

---
+++ x3/target/debug/deps/x3.d
@@ -0,0 +1,5 @@
+/Users/sandrozenatti/CLionProjects/TestFile/x3/target/debug/deps/x3: src/main.rs
+
+/Users/sandrozenatti/CLionProjects/TestFile/x3/target/debug/deps/x3.d: src/main.rs
+
+src/main.rs:
---
+++ x3/target/debug/x3.d
@@ -0,0 +1,1 @@
+/Users/sandrozenatti/CLionProjects/TestFile/x3/target/debug/x3: /Users/sandrozenatti/CLionProjects/TestFile/x3/src/main.rs
